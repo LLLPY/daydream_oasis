92fda5ba8c349e571d1b7bc584320d7a106af7e3
lll <2474605919@qq.com>
2023-09-12 14:14:49 +0800

    添加了response

--- daydream_oasis_backend/common/drf/response.py
+++ daydream_oasis_backend/common/drf/response.py
@@ -1,10 +1,27 @@
-import orjson
-from django.http import JsonResponse
+from rest_framework.response import Response
 
 
-class SucResponse(JsonResponse):
-    pass
+class SucResponse(Response):
+    CODE = "0"
+
+    def __init__(self, message='请求成功', data=None, error_code="", status=None, headers=None, content_type=None, **kwargs):
+        dic = {'code': self.CODE, 'message': message, 'data': {}, 'errorCode': error_code}
+        if data is not None:
+            dic['data'] = data
+
+        dic.update(kwargs)
+        super().__init__(data=dic, status=status, template_name=None,
+                         headers=headers, exception=False, content_type=content_type)
 
 
-class ErrResponse(JsonResponse):
-    pass
+class ErrResponse(Response):
+    CODE = "1"
+
+    def __init__(self, message='fail', data=None, error_code="", status=None, headers=None, content_type=None, **kwargs):
+        dic = {'code': self.CODE, 'message': message, 'data': {}, 'errorCode': error_code}
+        if data is not None:
+            dic['data'] = data
+
+        dic.update(kwargs)
+        super().__init__(data=dic, status=status, template_name=None,
+                         headers=headers, exception=False, content_type=content_type)
