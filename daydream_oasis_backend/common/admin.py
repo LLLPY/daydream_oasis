d8682b8979e77e19c4979751f7f3000e71fded0b
lll <2474605919@qq.com>
2023-09-12 13:54:32 +0800

    项目更名为daydream_oasis

---
+++ daydream_oasis_backend/common/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from daydream_oasis_backend.admin_site import my_site
+from common.models import BackgroundMusic
+
+
+# 构建一个公共基类，设置默认配置项
+class MyBaseAdmin:
+    save_on_top = True
+    save_on_bottom = True
+    list_select_related = True
+    save_as = True
+    list_display = ['id']
+
+
+# 背景音乐
+@admin.register(BackgroundMusic, site=my_site)
+class BackgroundMusicAdmin(admin.ModelAdmin, MyBaseAdmin):
+    list_display = ['id', 'title', 'avatar', 'path']
+    search_fields = ['title']
+    list_filter = ['title']
+
+    def save_model(self, request, obj, form, change):
+        obj.resize(obj.avatar.path, 50, 50)
+        super().save_model(request, obj, form, change)
