8655663673df2f51d129d789afbb999bec7edd2b
lll <2474605919@qq.com>
2023-11-18 17:28:18 +0800

    启动文件

--- config/setup.sh
+++ config/setup.sh
@@ -1,9 +1,14 @@
+
+
 #切换到虚拟环境
 source ../venv/bin/activate
 
-#启动uwsgi
+#杀死旧的uwsgi
 kill -9 $(lsof -i:8000)
+kill -9 $(lsof -i:6666)
+
+#启动新的uwsgi
 uwsgi uwsgi.ini
 
 #重载nginx
-service nginx reload
+systemctl reload nginx
