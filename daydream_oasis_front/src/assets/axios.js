53ac920460136f6730df70793e14cd1de000eb14
lll <2474605919@qq.com>
2023-11-18 17:27:29 +0800

    host修改

--- daydream_oasis_front/src/assets/axios.js
+++ daydream_oasis_front/src/assets/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axios_ins = axios.create({
     withCredentials: true,
-    baseURL: 'http://127.0.0.1:8000'
+    baseURL: 'http://www.lll.plus'
 
 })
 axios_ins.defaults.withCredentials = true;
