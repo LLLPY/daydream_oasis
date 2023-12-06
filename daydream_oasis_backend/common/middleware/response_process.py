2b89cd7db83a23e90ebc073abca2fccfac2ed6a3
lll <2474605919@qq.com>
2023-12-06 18:51:01 +0800

    解决了跨域携带cookie失效的问题

--- daydream_oasis_backend/common/middleware/response_process.py
+++ daydream_oasis_backend/common/middleware/response_process.py
@@ -12,6 +12,6 @@ class ResponseMiddleware(MiddlewareMixin):
         _uuid = request.COOKIES.get('uuid')
         if not _uuid:
             _uuid = str(uuid.uuid4())
-            response.set_cookie('uuid', _uuid, samesite='None', secure='None')
+            response.set_cookie('uuid', _uuid, samesite='', secure='', httponly='')
 
         return response
