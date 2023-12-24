ae2b3ec1b752d4fa1f69de18c855c8bce6019bb2
lll <2474705919@qq.com>
2023-12-24 20:09:33 +0800

    增加博客的md文件

--- daydream_oasis_front/docs/blog/[id].paths.js
+++ daydream_oasis_front/docs/blog/[id].paths.js
@@ -1,24 +1,24 @@
-import axios_ins from "../../src/assets/axios"
-
-export default {
-    async paths() {
-        let posts = await (await axios_ins(`/api/blog/?is_all=True`)).data.data.results
-        return posts.map((post) => {
-            return {
-                params: {
-                    id: post.id,
-                    title: post.title,
-                    author_username: post.author.username,
-                    pv: post.pv,
-                    read_times: post.read_times,
-                    read_time: post.read_time,
-                    category: post.category,
-                    tag_list: post.tag_list,
-                    create_time: post.create_time,
-                    update_time: post.update_time
-                },
-                content: post.content // raw Markdown or HTML
-            }
-        })
-    }
-}
+// import axios_ins from "../../src/assets/axios"
+//
+// export default {
+//     async paths() {
+//         let posts = await (await axios_ins(`/api/blog/?is_all=True`)).data.data.results
+//         return posts.map((post) => {
+//             return {
+//                 params: {
+//                     id: post.id,
+//                     title: post.title,
+//                     author_username: post.author.username,
+//                     pv: post.pv,
+//                     read_times: post.read_times,
+//                     read_time: post.read_time,
+//                     category: post.category,
+//                     tag_list: post.tag_list,
+//                     create_time: post.create_time,
+//                     update_time: post.update_time
+//                 },
+//                 content: post.content // raw Markdown or HTML
+//             }
+//         })
+//     }
+// }
