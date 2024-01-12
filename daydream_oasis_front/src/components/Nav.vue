dd344ef370e37111616be8df2d7e0724fce0117c
zeMing <duzm@chinachdu.com>
2024-01-12 13:56:58 +0800

    feat:登录插槽样式

--- daydream_oasis_front/src/components/Nav.vue
+++ daydream_oasis_front/src/components/Nav.vue
@@ -1,21 +1,48 @@
 <template>
-  <div>
-    <div v-if="!isLogin" class="login" @click="tapa">
-      <a href="/login">登录🚪</a>
+  <div class="nav">
+    <div class="login">
+      <div class="" @click="goLogin">{{ !isLogin && '登录🚪' || user }}</div>
     </div>
-    <div v-else class="user" @click="tapa">用户名称</div>
   </div>
-
 </template>
 
 <script setup>
 import { ref, unref } from 'vue'
 let isLogin = ref(false)
-const tapa=() => {
-  isLogin.value = !unref(isLogin)
+let user = ref()
+const goLogin=() => {
+  isLogin.value = true
+  initView()
+}
+
+const initView = () => {
+  user = '白日梦想圆'
 }
 </script>
 
 <style scoped>
+.nav {
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
 
+@media (min-width: 768px) {
+  .nav {
+    margin-left: 12px;
+
+    .login {
+      padding-left: 12px;
+    }
+
+    &::before {
+      content: '';
+      display: block;
+      clear: both;
+      width: 1px;
+      height: 24px;
+      background-color: var(--vp-c-divider);
+    }
+  }
+}
 </style>
