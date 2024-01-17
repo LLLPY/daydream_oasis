b7c74a1ffdc39a9ddda65349dc5abc3fbd9ba1d9
lll <2474705919@qq.com>
2024-01-17 22:33:00 +0800

    change:修改标签列表的样式

--- daydream_oasis_front/docs/.vitepress/theme/components/TagList.vue
+++ daydream_oasis_front/docs/.vitepress/theme/components/TagList.vue
@@ -1,139 +1,142 @@
 <template>
-    <div class="card" id="tag-box">
-        <div class="tools">
-            <div class="circle">
-                <span class="red box"></span>
-            </div>
-            <div class="circle">
-                <span class="yellow box"></span>
-            </div>
-            <div class="circle">
-                <span class="green box"></span>
-            </div>
-            <div class="more">
-                <span class=""><a href="/blog/1389.html">更多</a></span>
-            </div>
-        </div>
-        <div class="card__content">
-
-      <span class="tag" v-for="(tag, index) in tag_list" :key="index"
-            :style="{ backgroundColor:randomColor(),fontSize: randomSize() }">{{ tag.title }}</span>
-
-        </div>
+  <div class="card" id="tag-box">
+    <div class="tools">
+      <div class="circle">
+        <span class="red box"></span>
+      </div>
+      <div class="circle">
+        <span class="yellow box"></span>
+      </div>
+      <div class="circle">
+        <span class="green box"></span>
+      </div>
+      <div class="more">
+        <span class=""><a href="/blog/1389.html">更多</a></span>
+      </div>
     </div>
+    <div class="card__content">
+
+      <span class="tag" v-for="(tag, index) in tag_list" :key="index">{{ tag.title }}</span>
+
+    </div>
+  </div>
 
 
 </template>
 
 <script>
-    import axios_ins from '../assets/js/axios'
+import axios_ins from '../assets/js/axios'
 
-    export default {
-        data() {
-            return {
-                tag_list: []
-            };
-        },
-        methods: {
-            randomColor() {
-                // 生成随机颜色
-                const colors = ['#f43f5e', '#3b82f6', '#22c55e'];
-                const randomIndex = Math.floor(Math.random() * colors.length);
-                const randomColor = colors[randomIndex];
-                return randomColor
-            },
-            randomSize() {
-                // 生成随机字体大小
-                return Math.floor(Math.random() * 10 + 7) + 'px';
-            },
-            fetchTags() {
-                axios_ins.get('/api/tag/?format=json').then(response => {
-                    this.tag_list = response.data.data
-
-                }).catch(error => {
-                    console.log('error')
-                })
-            }
-        },
-        mounted() {
-            this.fetchTags();
-        }
+export default {
+  data() {
+    return {
+      tag_list: []
     };
+  },
+  methods: {
+    fetchTags() {
+      axios_ins.get('/api/tag/?format=json').then(response => {
+        this.tag_list = response.data.data
+      }).catch(error => {
+        console.log('error')
+      })
+    }
+  },
+  mounted() {
+    this.fetchTags();
+  }
+};
 </script>
 
 <style>
-    .card {
-        width: 100%;
-        margin: 0.5em auto;
-        border-radius: 3px;
-        border: 1px solid silver;
-        padding-bottom: 0.1em;
-        padding-left: 0.1em;
-        padding-right: 0.1em;
-        margin-bottom: 0;
-    }
+.card {
+  width: 100%;
+  margin: 0.5em auto;
+  border-radius: 3px;
+  border: 1px solid silver;
+  padding-bottom: 0.1em;
+  padding-left: 0.1em;
+  padding-right: 0.1em;
+  margin-bottom: 0;
+}
 
-    .tools {
-        align-items: center;
-        padding: 9px;
-    }
+.tools {
+  align-items: center;
+  padding: 9px;
+  padding-bottom: 0;
 
-    .circle {
-        display: inline-block;
-        padding: 0 4px;
-    }
+}
 
-    .box {
-        display: inline-block;
-        align-items: center;
-        width: 10px;
-        height: 10px;
-        padding: 1px;
-        border-radius: 50%;
-    }
+.circle {
+  display: inline-block;
+  padding: 0 4px;
+}
 
-    .red {
-        background-color: #ff605c;
-    }
+.box {
+  display: inline-block;
+  align-items: center;
+  width: 10px;
+  height: 10px;
+  padding: 1px;
+  border-radius: 50%;
+}
 
-    .yellow {
-        background-color: #ffbd44;
-    }
+.red {
+  background-color: #ff605c;
+}
 
-    .green {
-        background-color: #00ca4e;
-    }
+.yellow {
+  background-color: #ffbd44;
+}
 
-    .card__content .tag {
-        margin: 2px;
-        display: inline-block;
-        border-radius: 5px;
-        padding: 0.2em;
-        transition: background-color .3s linear;
-        font-weight: 600;
-        color: white;
-    }
+.green {
+  background-color: #00ca4e;
+}
 
-    .card__content .tag:hover {
-        cursor: pointer;
-        background-color: skyblue !important;
-    }
+.card__content {
+  text-align: justify;
+  word-break: break-all;
+  padding: 5px;
+  padding-bottom: 0;
 
-    .card .tools .more {
-        float: right;
-        font-size: 0.6em;
-    }
+  .tag {
+    margin-right: 5px;
+    font-size: 0.8rem;
+    border-radius: 2px;
+    padding: 2px 4px;
+    background-color: rgba(235, 235, 250, 0.7);
+  }
 
-    .card .tools .more:hover {
-        color: gray;
-    }
+  .tag:nth-child(1) {
+    margin-left: 0;
+  }
+}
 
-    .VPDocAside #tag-box {
-        max-height: 321px;
-        overflow: hidden;
-        box-shadow: 0 0 4px 0 rgba(0,0,0,.1);
 
-    }
+.card__content .tag:hover {
+  cursor: pointer;
+  background-color: skyblue !important;
+}
+
+.card .tools .more {
+  float: right;
+  font-size: 0.6em;
+}
+
+.card .tools .more:hover {
+  color: gray;
+}
+
+.VPDocAside #tag-box {
+  max-height: 318px;
+  overflow: hidden;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, .1);
+
+}
+
+a {
+  text-decoration: none !important;
+}
 
 
 </style>
