de7b4cb78075c50bcacc12c0a76f87b1af9292ee
lll <2474705919@qq.com>
2024-03-29 23:52:07 +0800

    fix:数据库的配置改为从环境变量获取

--- daydream_oasis_backend/daydream_oasis_backend/settings/settings_pro.py
+++ daydream_oasis_backend/daydream_oasis_backend/settings/settings_pro.py
@@ -1,18 +1,31 @@
 # -*- coding: UTF-8 -*-
 # @Author  ：LLL
 # @Date    ：2023/1/10 0:39
+import os
+
 from .base import *
 
 DEBUG = False
+
+# 从环境变量中获取数据库的配置
+db_NAME = os.environ.get('db_NAME')
+db_HOST = os.environ.get('db_HOST')
+db_PORT = os.environ.get('db_PORT')
+db_USER = os.environ.get('db_USER')
+db_PASSWORD = os.environ.get('db_PASSWORD')
+
+if not all((db_NAME, db_HOST, db_PORT, db_USER)):
+    raise Exception('数据库配置错误')
+
 # 数据库
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'daydream_oasis',
-        'HOST': 'www.lll.plus',
-        'PORT': '3306',
-        'USER': "daydream_oasis",
-        "PASSWORD": "daydream_oasis@lll",
+        'NAME': db_NAME,
+        'HOST': db_HOST,
+        'PORT': db_PORT,
+        'USER': db_USER,
+        "PASSWORD": db_PASSWORD,
         'OPTIONS': {
             'charset': 'utf8mb4',
             "init_command": "SET foreign_key_checks = 0;",
