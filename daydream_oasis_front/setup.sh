6a2aec54ab31d3ed13f4d1af697c964d5f700a29
lll <2474705919@qq.com>
2024-03-31 09:36:25 +0800

    chore:评论系统的配置文件

--- daydream_oasis_front/setup.sh
+++ daydream_oasis_front/setup.sh
@@ -13,6 +13,21 @@ wait
 
 # 启动评论系统
 kill -9 $(lsof -i:6870)
+# 写入discuss的配置文件
+echo "
+# Discuss environment Config
+# 启动的端口号(仅对服务器有用)
+DISCUSS_PORT=6870
+# Token 加密的密钥字符串([可选]自定义)
+DISCUSS_SECRET=discuss
+# 设置评论是否开启审核模式
+DISCUSS_AUDIT=false
+## 可选数据库 [mongodb, mysql, postgresql, sqlite, cloudbase]
+DISCUSS_DB_TYPE=mysql
+# ------ MySQL ------
+D_MYSQL_URL=mysql://$db_USER:$db_PASSWORD@db_HOST:$db_PORT/$db_NAME
+# 例子: mysql://username:password@host:port/database
+" > .env
 nohup node docs/discuss/index.cjs > setup.log &
 
 # 启动
